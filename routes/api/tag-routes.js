a317e2b6977d08b458ff6f2439934fa4ae72e553
qosole <williamj99wei@gmail.com>
2022-08-01 15:33:37 -0500

    Add GET route for /api/tags/:id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -25,6 +25,20 @@ router.get('/', async (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tag = await Tag.findByPk(req.params.id, {
+      include: [
+        { model: Product }
+      ]
+    });
+    if (!tag) {
+      res.status(404).json({ message: 'No tag found with that id '});
+      return;
+    }
+    res.status(200).json(tag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
