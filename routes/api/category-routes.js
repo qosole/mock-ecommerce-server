ff27511193aa1c03fa2d45977c5152cda5c4476a
qosole <williamj99wei@gmail.com>
2022-08-01 14:00:43 -0500

    Add GET route for all categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,21 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const allCategories = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    if (!allCategories) {
+      res.status(404).json({ message: 'No category data in db' });
+      return;
+    }
+    res.status(200).json(allCategories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
